18283c5fe5cf17f9868ffa30e0adff3fac9db58d
Danilo Diniz <71573558+xdnzz@users.noreply.github.com>
2021-03-22 16:34:54 -0300

    Add files via upload
    Mais um projeto do curso de React do 0 ao avançado. Os arquivos devem(ou não) ser importados. Em cada arquivo tem os códigos de onde cada um está sendo importado, caso queiram usar o código, criem uma pasta ou mudem o caminho de import de cada arquivo.

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './style.css'
+import Routes from './routes'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <Routes/>  
+      </div>
+    )
+  }
+}
+
+
+
+export default App
